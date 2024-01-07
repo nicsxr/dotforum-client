1f1c0766e4dbe9590850f6dbff784da77fe40ea5
Nikoloz Skhirtladze <36042283+nicsxr@users.noreply.github.com>
2024-01-07 19:50:09 +0400

    prod api

--- src/main.jsx
+++ src/main.jsx
@@ -14,6 +14,11 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   </React.StrictMode>,
 )
 
+if (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') {
+  axios.defaults.baseURL = 'https://localhost:7177/api' //HTTPS
+  // axios.defaults.baseURL = 'http://192.168.0.161:5023/api' //HTTP
+  console.log(proc)
+} else {
+  axios.defaults.baseURL = 'https://dotforum.azurewebsites.net/api'
+}
 axios.defaults.withCredentials = true
-axios.defaults.baseURL = 'https://localhost:7177/api' //HTTPS
-// axios.defaults.baseURL = 'http://192.168.0.161:5023/api' //HTTP
