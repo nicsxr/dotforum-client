d8d5dd2aa338bd7fc74605927f41740d2f2f90ca
Nikoloz Skhirtladze <36042283+nicsxr@users.noreply.github.com>
2024-06-14 01:04:01 +0400

    https

--- src/main.jsx
+++ src/main.jsx
@@ -14,6 +14,6 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   </React.StrictMode>,
 )
 
-axios.defaults.baseURL = process.env.API_URL ?? "http://eogk4s8.164.92.249.9.sslip.io"
+axios.defaults.baseURL = process.env.API_URL ?? "https://eogk4s8.164.92.249.9.sslip.io:3001/api"
 
 axios.defaults.withCredentials = true
