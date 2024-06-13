edcb71d6cc2a214293c75bfb21bb017f73c5936b
Nikoloz Skhirtladze <36042283+nicsxr@users.noreply.github.com>
2024-06-14 02:05:22 +0400

    ignore cert

--- src/main.jsx
+++ src/main.jsx
@@ -14,6 +14,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   </React.StrictMode>,
 )
 
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
 axios.defaults.baseURL = process.env.API_URL ?? "https://eogk4s8.164.92.249.9.sslip.io:3001/api"
 
 axios.defaults.withCredentials = true
